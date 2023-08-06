6c7cbe1c4552c8b48ed09464318dbf17e889d13f
wangjianghai <654499437@qq.com>
2023-08-06 10:13:48 +0800

    init

--- dbhelp-service-api/src/main/java/com/dbhelp/DbHelpApiApplication.java
+++ dbhelp-service-api/src/main/java/com/dbhelp/DbHelpApiApplication.java
@@ -1,5 +1,6 @@
 package com.dbhelp;
 
+import kim.sesame.common.web.context.SpringContextUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DbHelpApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(DbHelpApiApplication.class, args);
-//        SpringContextUtil.printStartInfo();
+        SpringContextUtil.printStartInfo();
     }
 
 }
